aabfeeae8f0f9766c5a5793e76ac2dacf5c9cb82
black hat developer <idevr@icloud.com>
2025-03-10 12:11:00 -0400

    Add mobile-banking-app/App.js 1741623060829

---
+++ mobile-banking-app/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import AppNavigator from './src/navigation/AppNavigator';
+import { AuthProvider } from './src/context/AuthContext';
+
+const App = () => {
+  return (
+    <SafeAreaProvider>
+      <AuthProvider>
+        <NavigationContainer>
+          <AppNavigator />
+        </NavigationContainer>
+      </AuthProvider>
+    </SafeAreaProvider>
+  );
+};
+
+export default App;
